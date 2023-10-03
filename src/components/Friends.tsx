b8132cad425a5012edf374c51c64a6316d4be24d
Jaideep <guntupallijaideep@gmail.com>
2023-10-03 11:46:20 +0530

    Added Placeholder components to Dashboard

---
+++ src/components/Friends.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Friends = () => {
+    return (
+        <div className="col-span-3">
+            <h1 className="my-8 text-2xl font-semibold tracking-tight">
+                Friends
+            </h1>
+        </div>
+    );
+};
+
+export default Friends;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,9 +14,9 @@ const Navbar = () => {
     const user = getUser();
 
     return (
-        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b bg-white/75 dark:bg-background backdrop-blur-lg transition-all">
             <WidthWrapper>
-                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                <div className="flex h-14 items-center justify-between border-b">
                     <Link href="/" className="flex z-40 font-semibold px-4">
                         <span className="text-xl px-3">huddl.</span>
                     </Link>
